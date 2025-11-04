6cd3f3a27c5019aff5f2a0b3167527edef1024fd
Isac <hanccoismael07@gmail.com>
2025-11-03 19:10:07 -0500

    Respaldo de sidebar

--- resources/views/livewire/catalog/index.blade.php
+++ resources/views/livewire/catalog/index.blade.php
@@ -1,4 +1,4 @@
 <div class="p-8">
             <h1 class="text-2xl font-bold">📚 Catálogo del Alumno</h1>
             <p class="mt-2 text-zinc-500">Aquí irá la vista pro tipo “My Books” (búsqueda, populares, etc.).</p>
-        </div>
+</div>
